ecc800e5261b820887e0b56687c92784ccfe2b01
Vinicius Batista <viniciusbfs2012@gmail.com>
2018-05-15 10:43:38 -0300

    user: add change password to SettingsModal

--- src/support/mixins/rules.js
+++ src/support/mixins/rules.js
@@ -2,6 +2,11 @@ import { test, length, pickAll } from 'ramda'
 import { isNotEmpty } from '@/helpers/isNotEmpty'
 
 const emailRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+const passwordRules = [
+  v => isNotEmpty(v) || 'Password is required',
+  v => length(v) >= 6 || 'Password must have at least 6 caracters.'
+]
+
 const rulesObject = {
   email: [
     v => isNotEmpty(v) || 'Email is required',
@@ -10,10 +15,9 @@ const rulesObject = {
   name: [v => isNotEmpty(v) || 'Name is required'],
   description: [v => isNotEmpty(v) || 'Description is required'],
   username: [v => isNotEmpty(v) || 'Username is required'],
-  password: [
-    v => isNotEmpty(v) || 'Password is required',
-    v => length(v) >= 6 || 'Password must have at least 6 caracters.'
-  ]
+  password: passwordRules,
+  oldPassword: passwordRules,
+  newPassword: passwordRules
 }
 
 export default (rules) => ({
