09bf8ea37bb648ba766ea96d52235e93bfe12b45
Vinicius Batista <viniciusbfs2012@gmail.com>
2018-05-15 10:44:14 -0300

    user: add change password to SettingsModal

--- src/app/user/components/SettingsModal.vue
+++ src/app/user/components/SettingsModal.vue
@@ -1,14 +1,78 @@
 <template>
   <v-dialog v-model="isDialogOpen" full-width max-width="500">
     <SideBarListItem slot="activator" :icon="'settings'" :title="'Settings'"/>
+    <v-card class="pa-3">
+      <v-form v-model="valid" @submit.prevent="handleSubmit" ref="form">
+        <v-card-title class="headline">Change password</v-card-title>
+        <v-card-text>
+          <v-alert type="error" v-model="hasError">
+            {{ errorMessage }}
+          </v-alert>
+          <v-text-field v-for="{ label, model } in fields"
+            type="password"
+            :key="model"
+            :label="label"
+            v-model="input[model]"
+            :rules="rules[model]"
+          />
+        </v-card-text>
+        <FormModalActions
+          :sendDisabled="!valid"
+          @cancelClick="closeDialog"
+        />
+      </v-form>
+    </v-card>
   </v-dialog>
 </template>
 
 <script>
-import { modal } from '@/support/mixins'
+import { modal, rules, handleErrors } from '@/support/mixins'
+import FormModalActions from '@/components/FormModalActions'
+import changePasswordMutation from '../graphql/changePassword.gql'
 
 export default {
   name: 'SettingsModal',
-  mixins: [modal]
+  components: {
+    FormModalActions
+  },
+  mixins: [
+    modal,
+    handleErrors,
+    rules(['newPassword', 'oldPassword'])
+  ],
+  data: () => ({
+    valid: '',
+    input: {
+      newPassword: '',
+      oldPassword: ''
+    },
+    fields: [
+      {
+        label: 'Old Password',
+        model: 'oldPassword'
+      },
+      {
+        label: 'New Password',
+        model: 'newPassword'
+      }
+    ]
+  }),
+  methods: {
+    handleSubmit () {
+      return this
+        .$apollo
+        .mutate({
+          mutation: changePasswordMutation,
+          variables: { input: this.input }
+        })
+        .then(this.resetForm)
+        .then(this.closeDialog)
+        .catch(this.handleError)
+    },
+    resetForm () {
+      this.$refs.form.reset()
+      this.hasError = false
+    }
+  }
 }
 </script>
